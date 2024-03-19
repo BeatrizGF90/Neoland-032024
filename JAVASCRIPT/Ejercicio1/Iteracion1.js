6b54cf662a79752bf7269bd513aa210f0cdcbdfd
BeatrizGF90 <beatrizgf1990@gmail.com>
2024-03-19 17:10:09 +0100

    Ejercicios 1

---
+++ JAVASCRIPT/Ejercicio1/Iteracion1.js
@@ -0,0 +1,19 @@
+// control + k + c --> para comentar
+// control + k + u --> para descomentar
+// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+// 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+// 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+// 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+
+let myFavoriteHero = "Hulk";
+console.log("Mi primer consoleg", myFavoriteHero);
+
+let x = 50;
+console.log(`El valor de x es ${x}`);
+
+let h = 5;
+let y = 10;
+console.log(`El valor de h e y son respectivamente ${h} y ${y}`);
+
+let z = h + y;
+console.log(`El valor de sumar h e y es ${z}`);
