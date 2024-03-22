e0669b7519fbb5ea27e755772de284a7aa6d9e95
BeatrizGF90 <beatrizgf1990@gmail.com>
2024-03-22 09:36:40 +0100

    Funciones

---
+++ JAVASCRIPT/Ejercicio2Funciones/Iteracion6.js
@@ -0,0 +1,20 @@
+// Crea una función que reciba por parámetro un array y compruebe si existen
+// elementos duplicados, en caso que existan los elimina para retornar un array
+//  sin los elementos duplicados.
+
+const duplicates = [
+  "sushi",
+  "pizza",
+  "burger",
+  "potatoe",
+  "pasta",
+  "ice-cream",
+  "pizza",
+  "chicken",
+  "onion rings",
+  "pasta",
+  "soda",
+];
+const removeDuplicates = (param) => {
+  // insert code
+};
