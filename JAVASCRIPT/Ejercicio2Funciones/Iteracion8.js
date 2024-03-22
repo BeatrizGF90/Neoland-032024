f8ae33831a2d14e6595f68e740176f3d8df9831b
BeatrizGF90 <beatrizgf1990@gmail.com>
2024-03-22 19:04:58 +0100

    Ejercicio 8 funciones

--- JAVASCRIPT/Ejercicio2Funciones/Iteracion8.js
+++ JAVASCRIPT/Ejercicio2Funciones/Iteracion8.js
@@ -17,17 +17,22 @@ const counterWords = [
 const repeatCounter = (param) => {
   const totalPalabrasRepes = [];
   let sumaPalabrasRepes = 0;
-  const result = [];
+  const result = []; // en esta variable voy a guardar la palabra repetida y al lado el número de veces que aparece esa palabra.
   for (let i = 0; i < param.length; i++) {
     !totalPalabrasRepes.includes(param[i]) && totalPalabrasRepes.push(param[i]);
   }
   console.log(totalPalabrasRepes);
   for (let x = 0; x < totalPalabrasRepes.length; x++) {
-    sumaPalabrasRepes = 0;
+    sumaPalabrasRepes = 0; // Por si acaso vuelvo a poner la variable sumar a 0.
     for (let z = 0; z < param.length; z++) {
+      /** Para cada valor del nuevo array de no repetidas, vuelvo a recorrer el array inicial
+       * y cada vez que coincida la palabra con la que estoy comprobando, incremento la variable suma.
+       */
+
       totalPalabrasRepes[x] == param[z] && sumaPalabrasRepes++;
     }
     result.push(`${totalPalabrasRepes[x]}, ${sumaPalabrasRepes}`);
+    // A la variable result le añado la palabra que estoy analizando + el número de veces que sale repetida.
   }
   return result;
 };
