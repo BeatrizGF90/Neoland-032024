421c9fb7aa4f5cb7f4be4b3d61d4b117dfcb2624
BeatrizGF90 <beatrizgf1990@gmail.com>
2024-03-22 18:55:22 +0100

    Ejercicio 8 funciones

--- JAVASCRIPT/Ejercicio2Funciones/Iteracion8.js
+++ JAVASCRIPT/Ejercicio2Funciones/Iteracion8.js
@@ -14,6 +14,22 @@ const counterWords = [
   "upgrade",
   "code",
 ];
-function repeatCounter(param) {
-  // insert code
-}
+const repeatCounter = (param) => {
+  const totalPalabrasRepes = [];
+  let sumaPalabrasRepes = 0;
+  const result = [];
+  for (let i = 0; i < param.length; i++) {
+    !totalPalabrasRepes.includes(param[i]) && totalPalabrasRepes.push(param[i]);
+  }
+  console.log(totalPalabrasRepes);
+  for (let x = 0; x < totalPalabrasRepes.length; x++) {
+    sumaPalabrasRepes = 0;
+    for (let z = 0; z < param.length; z++) {
+      totalPalabrasRepes[x] == param[z] && sumaPalabrasRepes++;
+    }
+    result.push(`${totalPalabrasRepes[x]}, ${sumaPalabrasRepes}`);
+  }
+  return result;
+};
+const palabrasRepetidas = repeatCounter(counterWords);
+console.log(palabrasRepetidas);
