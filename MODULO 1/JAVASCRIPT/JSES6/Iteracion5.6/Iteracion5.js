dfdf668942cfe6695d98e9561c0a344a90a22fdb
BeatrizGF90 <beatrizgf1990@gmail.com>
2024-04-03 09:28:45 +0200

    CSS3 hasta Flex Layout

---
+++ MODULO 1/JAVASCRIPT/JSES6/Iteracion5.6/Iteracion5.js
@@ -0,0 +1,46 @@
+// 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola
+// los streamers que incluyan la palabra introducida en el input. De esta forma, si
+// introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+// introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+const streamers3 = [
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
+];
+
+const input = document.querySelector("[data-function=toFilterStreamers]");
+
+const filterString = (inputValue) => {
+  const nuevoStreamers3 = streamers3.filter((user) =>
+    user.name.toLowerCase().includes(inputValue)
+  );
+  // para que solo me saque el nombre del streamer hago un for of.
+  for (let streamer of nuevoStreamers3) {
+    console.log(streamer.name);
+  }
+};
+
+input.addEventListener("change", (evento) => {
+  filterString(evento.target.value);
+});
+
+// Otra manera:
+// const input = document.getElementById("texoABuscar");
+// const resultados = document.getElementById("resultados");
+
+// const streamers2 = [
+//   { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+//   { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+//   { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+//   { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
+// ];
+
+// input.addEventListener("input", (e) => {
+//   e.preventDefault();
+//   const streamers2Buscador = streamers2.filter((player) =>
+//     player.name.toLowerCase().includes(input.value)
+//   );
+//   console.log(streamers2Buscador);
+//   resultados.innerHTML = streamers2Buscador.map((element) => element.name); //con esto lo que hago es inyectarlo en el html, y con el .map recorro el array y saco el  .name
+// });
---
+++ MODULO 1/JAVASCRIPT/JSES6/Iteracion5.7/Iteracion57.js
@@ -0,0 +1,21 @@
+/* 5.7 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si introduzco 'i', 
+me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+En este caso, muestra solo los streamers filtrados cuando hagamos click en el button. */
+const input = document.getElementById("texto");
+const button = document.getElementById("button");
+
+const streamers4 = [
+  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
+  { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
+  { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
+];
+
+button.addEventListener("click", (e) => {
+  const nuevoStreamers4 = streamers4.filter((user) =>
+    user.name.toLowerCase().includes(input.value)
+  );
+  console.log(nuevoStreamers4);
+});
